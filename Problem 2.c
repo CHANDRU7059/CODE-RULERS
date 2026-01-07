a8f44896a1ab7cc62b8b42ee9af864c3242b45a1
CHANDRU7059 <chandru.7059@gmail.com>
2026-01-07 16:44:30 +0530

    Update Problem 2.c

--- Problem 2.c
+++ Problem 2.c
@@ -1,23 +1,30 @@
-#include<stdio.h>
-int main()
-{
-    int n,a,r,p,rc;
-    printf("Enter the total no of classes :");
-    scanf("%d",&n);
-    printf("Enter the no of classes attended:");
-    scanf("%d",&a);
-    p=(a/n)*100;
-    r=0.75*n;
-    printf("\nAttendance percentage:");
-    printf(p);
-    if(p>=75)
-    {
-        printf("\nstatus : Eligible");
-    }else
-    {printf("\nstatus : Not eligible");
-        rc=r-a;
-        printf("\nAdditional classes required:");
-        printf(rc);
+
+#include <stdio.h>
+
+int main() {
+    int n, a, r, rc;
+    float p;
+
+    printf("Enter the total no of classes: ");
+    scanf("%d", &n);
+
+    printf("\nEnter the no of classes attended: ");
+    scanf("%d", &a);
+
+    p = ((float)a / n) * 100;
+
+    r = 0.75 * n;
+
+    printf("\nAttendance percentage: %.2f", p);
+
+    if (p >= 75) {
+        printf("\nStatus: Eligible");
+    } else {
+        printf("\nStatus: Not eligible");
+        rc = r - a;
+        printf("\nAdditional classes required: %d", rc);
     }
- return 0;
+
+    return 0;
 }
+
